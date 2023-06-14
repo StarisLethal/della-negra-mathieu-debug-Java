0631757e5ca1af59ce632a211c212fa066433f9a
Amenor <lethality.game@gmail.com>
2023-06-14 12:37:23 +0200

    JavaDoc Done in theory

---
+++ Project02Eclipse/javadoc/member-search-index.js
@@ -0,0 +1,39 @@
+memberSearchIndex = [{
+    "p": "com.hemebiotech.analytics",
+    "c": "AnalyticsCounter",
+    "l": "AnalyticsCounter()",
+    "u": "%3Cinit%3E()"
+}, {
+    "p": "com.hemebiotech.analytics",
+    "c": "Application",
+    "l": "Application()",
+    "u": "%3Cinit%3E()"
+}, {"p": "com.hemebiotech.analytics", "c": "ISymptomsReader", "l": "GetSymptoms()"}, {
+    "p": "com.hemebiotech.analytics",
+    "c": "ReadSymptomsDataFromFile",
+    "l": "GetSymptoms()"
+}, {
+    "p": "com.hemebiotech.analytics",
+    "c": "Application",
+    "l": "main(String[])",
+    "u": "main(java.lang.String[])"
+}, {
+    "p": "com.hemebiotech.analytics",
+    "c": "ReadSymptomsDataFromFile",
+    "l": "ReadSymptomsDataFromFile(String)",
+    "u": "%3Cinit%3E(java.lang.String)"
+}, {
+    "p": "com.hemebiotech.analytics",
+    "c": "ISymptomsWriter",
+    "l": "WriteSymptoms()"
+}, {
+    "p": "com.hemebiotech.analytics",
+    "c": "WriteSymptomsDataToFile",
+    "l": "WriteSymptoms()"
+}, {
+    "p": "com.hemebiotech.analytics",
+    "c": "WriteSymptomsDataToFile",
+    "l": "WriteSymptomsDataToFile(TreeMap<String, Integer>)",
+    "u": "%3Cinit%3E(java.util.TreeMap)"
+}];
+updateSearchResults();
